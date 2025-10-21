01fa82a61e2be36bc8a49b66dc458b0d503c4886
CheetaiTan <taicheetan@gmail.com>
2025-10-21 17:02:16 +0800

    fix bug

--- src/main/java/com/comp2042/Main.java
+++ src/main/java/com/comp2042/Main.java
@@ -4,29 +4,37 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.media.AudioClip;
 import javafx.stage.Stage;
-
-import java.net.URL;
-import java.util.ResourceBundle;
 
 public class Main extends Application {
 
+    private static AudioClip bgMusic; // stays alive across scenes
+
     @Override
     public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/StartMenu.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root, 500, 600);
 
-        URL location = getClass().getClassLoader().getResource("gameLayout.fxml");
-        ResourceBundle resources = null;
-        FXMLLoader fxmlLoader = new FXMLLoader(location, resources);
-        Parent root = fxmlLoader.load();
-        GuiController c = fxmlLoader.getController();
-
-        primaryStage.setTitle("TetrisJFX");
-        Scene scene = new Scene(root, 300, 510);
+        primaryStage.setTitle("Tetris Game");
         primaryStage.setScene(scene);
         primaryStage.show();
-        new GameController(c);
+
+        // 🎵 Play background music globally
+        bgMusic = new AudioClip(getClass().getResource("/sounds/background.mp3").toString());
+        bgMusic.setCycleCount(AudioClip.INDEFINITE);
+        bgMusic.setVolume(0.3);
+        bgMusic.play();
     }
 
+    public static void stopMusic() {
+        if (bgMusic != null) bgMusic.stop();
+    }
+
+    public static void startMusic() {
+        if (bgMusic != null && !bgMusic.isPlaying()) bgMusic.play();
+    }
 
     public static void main(String[] args) {
         launch(args);
